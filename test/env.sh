42f4a88ed1cb505550d1b26334ad1677024694f5
Robert Escriva <robert@rescrv.net>
2016-03-03 10:16:48 -0500

    A test shim in testing that removes randomness
    This means that added caveats will be deterministic, and thus testable, without
    having to alter the underlying code.

--- test/env.sh
+++ test/env.sh
@@ -2,4 +2,5 @@ export MACAROONS_SRCDIR="$1"
 export MACAROONS_BUILDDIR="$2"
 export MACAROONS_VERSION="$3"
 
+export LD_PRELOAD="${MACAROONS_BUILDDIR}/.libs/libmacaroons-shim.so"
 export PYTHONPATH="${MACAROONS_BUILDDIR}"/bindings/python/.libs:${PYTHONPATH}
